a0ef8f8e74b080516bd0cd18d07c2f215dfaa041
Corina Perjan <cperjan@gmail.com>
2022-04-11 22:20:31 +0200

    done

--- js/index.js
+++ js/index.js
@@ -40,6 +40,10 @@ if(hacker1<hacker2){
     {console.log(`Yo, the navigator goes first definitely.`)} 
     else{console.log(`wrong`)}
 
-// Bonus 1: Palindromes
+// Bonus 1: Lorem ipsum counter
+let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+let countWords=text.split(" et ").length - 1;
+console.log(countWords);
 
-// Bonus 2: Lorem ipsum counter
+// Bonus 2: Palindromes
+
